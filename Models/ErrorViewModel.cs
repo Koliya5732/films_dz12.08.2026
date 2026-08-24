b2b5135f7ad9ac56c2e209ca437e7f21543ddb60
User <User@DESKTOP-LO7QQED>
2026-08-24 19:41:48 +0300

    Добавьте файлы проекта.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace films_dz12._08._2026.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Models/Movie.cs
@@ -0,0 +1,12 @@
+﻿namespace films_dz12._08._2026.Models
+{
+    public class Movie
+    {
+        public string Title { get; set; }
+        public string Director { get; set; }
+        public string Genre { get; set; }
+        public int Year { get; set; }
+        public string Poster { get; set; }
+        public string Description { get; set; }
+    }
+}
